95d8d168fa5b63dc16f2d518e6cde472ab19fab9
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-18 22:28:07 +0545

    Completed the special offer section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,17 @@
+import ServiceCard from "../components/ServiceCard";
+import { services } from "../constants";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section
+      id="services"
+      className="max-container flex justify-center flex-wrap gap-9"
+    >
+      {services.map((service) => (
+        <ServiceCard key={service.label} service={service} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
