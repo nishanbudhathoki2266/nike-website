f1a47a2594ef2f1323473a47ff9ba8baf2fe4408
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-18 20:26:59 +0545

    Added all basic sections into a directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 function App() {
-  return <main className="relative">Nike - Just do it</main>;
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">Popular Products</section>
+      <section className="padding">Super Quality</section>
+      <section className="padding padding-x py-10">Services</section>
+      <section className="padding">SpecialOffers</section>
+      <section className="padding bg-pale-blue">Customer Reviews</section>
+      <section className="padding sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="padding bg-black padding-x padding-t pb-8">
+        Footer
+      </section>
+    </main>
+  );
 }
 
 export default App;
