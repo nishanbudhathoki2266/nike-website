23b5299ac4e58c07a53506247f85d60bb54bc718
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-18 20:48:20 +0545

    Completed the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
+import Nav from "./components/Nav";
+import CustomerReviews from "./sections/CustomerReviews";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import Services from "./sections/Services";
+import SpecialOffers from "./sections/SpecialOffers";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
+
 function App() {
   return (
     <main className="relative">
-      Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding">Popular Products</section>
-      <section className="padding">Super Quality</section>
-      <section className="padding padding-x py-10">Services</section>
-      <section className="padding">SpecialOffers</section>
-      <section className="padding bg-pale-blue">Customer Reviews</section>
-      <section className="padding sm:py-32 py-16 w-full">Subscribe</section>
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
       <section className="padding bg-black padding-x padding-t pb-8">
-        Footer
+        <Footer />
       </section>
     </main>
   );
